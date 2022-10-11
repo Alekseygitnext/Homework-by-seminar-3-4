177a6ebf82cb6766fb3f7bf462eb1d66287e282a
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-11 17:15:17 +1000

    задача 1 к семинару №3

--- task1_by seminar №3/Program.cs
+++ task1_by seminar №3/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введите число: ");
+int chislo = int.Parse(Console.ReadLine() ?? "0");
+int count = 1;
+while (count <= chislo)
+{
+    /*double res = Math.Pow(count, 3);*/
+    int res = count*count*count;
+    Console.WriteLine($"{res} является кубом {count}");
+    count++;
+}
+
