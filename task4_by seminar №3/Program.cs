fc2a371ebae95fbf1ca5c89d3e808d7f7bd0c86b
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-11 19:25:37 +1000

    Задача 4 к семинару № 3

---
+++ task4_by seminar №3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать кубы чисел заканчивающиеся на чётную цифру
+Console.Write("Введите число: ");
+int Chislo = int.Parse(Console.ReadLine() ?? "0");
+int Kub;
+Console.WriteLine("Кубами заканчивающемися на чётную цифру являются: ");
+for (int i=1; i<=Chislo;i++)
+{
+Kub = i*i*i;
+if ((Kub%10)%2==0)
+Console.Write($"{Kub} ");
+};
