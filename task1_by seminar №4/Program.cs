9fcda9e37dfb2009644add9cd8ab88a392da2c4d
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-11 19:42:34 +1000

    Задача 1 к семинару № 4

---
+++ task1_by seminar №4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами, вывести их на экран
+void FillArray(int[] massiv)
+{
+    Random Numbers = new Random();
+        for(int ind = 0;ind<massiv.Length;ind++)
+    {
+        massiv[ind] = Numbers.Next(0,2);
+    }
+}
+void PrintArray(int[] massiv)
+{
+    foreach (var item in massiv)
+    {
+        Console.Write($"{item} ");
+    }
+}
+int[] Array = new int[8];
+FillArray(Array);
+PrintArray(Array);
