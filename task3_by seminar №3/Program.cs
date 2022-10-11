5597b51bd7b8369a868c4209959c79d40ce2080d
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-11 18:27:04 +1000

    Задача 3 к семинару № 3

---
+++ task3_by seminar №3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число: ");
+int Chislo = int.Parse(Console.ReadLine() ?? "0");
+int result = 1;
+for (int i=1; i<=Chislo;i++)
+{
+result = result * i;
+Console.WriteLine(result);
+}
